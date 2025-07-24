47cad0bea6ba4838d634e4fa132aa40e1eb293b3
Mustafa Hisham <57047334+mustafahischam@users.noreply.github.com>
2025-07-24 19:17:56 +0300

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '' : '/',
+  base: command === 'build' ? '/Start-Framework/' : '/',
   plugins: [react()],
 }));
